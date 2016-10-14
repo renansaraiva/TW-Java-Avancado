3e980e6f3e8f8f928d0d832cb08d90bbc42b0dcc
Renan Saraiva <renan.saraiva@gmail.com>
2016-10-14 17:08:50 -0300

    22% - Criando a primeira action

---
+++ treinaweb-teste-struts2/src/main/java/br/com/treinaweb/teste/struts2/login/actions/LoginAction.java
@@ -0,0 +1,31 @@
+package br.com.treinaweb.teste.struts2.login.actions;
+
+public class LoginAction {
+
+	private String email;
+	private String senha;
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String execute() {
+		if (this.email.equals("teste@teste.com") && this.senha.equals("123")) {
+			return "SUCCESS";
+		} else {
+			return "ERROR";
+		}
+	}
+}
