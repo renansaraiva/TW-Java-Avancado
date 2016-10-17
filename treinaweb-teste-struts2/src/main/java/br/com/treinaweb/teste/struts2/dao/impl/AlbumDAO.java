a34e8cd4017bde6e8022a6dccd2925d43ff2fedf
Renan Saraiva <renan.saraiva@gmail.com>
2016-10-17 15:02:16 -0200

    40% - Implementando os métodos todos() e porId()

--- treinaweb-teste-struts2/src/main/java/br/com/treinaweb/teste/struts2/dao/impl/AlbumDAO.java
+++ treinaweb-teste-struts2/src/main/java/br/com/treinaweb/teste/struts2/dao/impl/AlbumDAO.java
@@ -2,21 +2,34 @@ package br.com.treinaweb.teste.struts2.dao.impl;
 
 import java.util.List;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
 import br.com.treinaweb.teste.struts2.dao.interfaces.IDAOGenerico;
+import br.com.treinaweb.teste.struts2.hibernate.util.HibernateSessionFactoryUtils;
 import br.com.treinaweb.teste.struts2.models.Album;
 
 public class AlbumDAO implements IDAOGenerico<Album, Integer> {
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public List<Album> todos() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Album> albuns = null;
+		SessionFactory sf = HibernateSessionFactoryUtils.getSessionFactory();
+		Session session = sf.openSession();
+		albuns = (List<Album>)session.createQuery("from Album").list();
+		session.close();
+		return albuns;
 	}
 
 	@Override
 	public Album porId(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		Album album = null;
+		SessionFactory sf = HibernateSessionFactoryUtils.getSessionFactory();
+		Session session = sf.openSession();
+		album = session.get(Album.class, id);
+		session.close();
+		return album;
 	}
 
 	@Override
