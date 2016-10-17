3b402494d887c81b6bee92b59bd2f5210be54f20
Renan Saraiva <renan.saraiva@gmail.com>
2016-10-17 10:32:40 -0200

    35% - Criando nosso primeiro model

---
+++ treinaweb-teste-struts2/src/main/java/br/com/treinaweb/teste/struts2/models/Album.java
@@ -0,0 +1,52 @@
+package br.com.treinaweb.teste.struts2.models;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "ALB_ALBUNS")
+public class Album implements Serializable{
+
+	private static final long serialVersionUID = -1327114278435446441L;
+	
+	@Id
+	@GeneratedValue
+	@Column(name = "ALB_ID")
+	private int id;
+	
+	@Column(name = "ALB_NOME")
+	private String nome;
+	
+	@Column(name = "ALB_ANO_LANCAMENTO")
+	private int ano;
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getAno() {
+		return ano;
+	}
+
+	public void setAno(int ano) {
+		this.ano = ano;
+	}
+
+}
