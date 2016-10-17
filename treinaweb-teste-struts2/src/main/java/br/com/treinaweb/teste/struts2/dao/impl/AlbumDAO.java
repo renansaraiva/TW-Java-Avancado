ad35b1832052caae8ac638469a98a8f05ff6b746
Renan Saraiva <renan.saraiva@gmail.com>
2016-10-17 14:13:13 -0200

    39% - Iniciando a primeira implementação de nosso DAO com Hibernate

---
+++ treinaweb-teste-struts2/src/main/java/br/com/treinaweb/teste/struts2/dao/impl/AlbumDAO.java
@@ -0,0 +1,41 @@
+package br.com.treinaweb.teste.struts2.dao.impl;
+
+import java.util.List;
+
+import br.com.treinaweb.teste.struts2.dao.interfaces.IDAOGenerico;
+import br.com.treinaweb.teste.struts2.models.Album;
+
+public class AlbumDAO implements IDAOGenerico<Album, Integer> {
+
+	@Override
+	public List<Album> todos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Album porId(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void inserir(Album modelo) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void atualizar(Album modelo) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deletar(Album modelo) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+}
