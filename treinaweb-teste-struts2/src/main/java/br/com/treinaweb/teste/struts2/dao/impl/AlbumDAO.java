812b0f7d86d5521617893a3e776f3bdb232d0929
Renan Saraiva <renan.saraiva@gmail.com>
2016-11-01 17:11:02 -0200

    61% - Refatorando o AlbumDAO para utilizar a JPA

--- treinaweb-teste-struts2/src/main/java/br/com/treinaweb/teste/struts2/dao/impl/AlbumDAO.java
+++ treinaweb-teste-struts2/src/main/java/br/com/treinaweb/teste/struts2/dao/impl/AlbumDAO.java
@@ -2,12 +2,11 @@ package br.com.treinaweb.teste.struts2.dao.impl;
 
 import java.util.List;
 
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 import br.com.treinaweb.teste.struts2.dao.interfaces.IDAOGenerico;
-import br.com.treinaweb.teste.struts2.hibernate.util.HibernateSessionFactoryUtils;
 import br.com.treinaweb.teste.struts2.models.Album;
 
 public class AlbumDAO implements IDAOGenerico<Album, Integer> {
@@ -16,51 +15,115 @@ public class AlbumDAO implements IDAOGenerico<Album, Integer> {
 	@Override
 	public List<Album> todos() {
 		List<Album> albuns = null;
-		SessionFactory sf = HibernateSessionFactoryUtils.getSessionFactory();
-		Session session = sf.openSession();
-		albuns = (List<Album>)session.createQuery("from Album").list();
-		session.close();
+		/* 
+		 * UTILIZANDO HIBERNATE
+		 * 
+		 * SessionFactory sf = HibernateSessionFactoryUtils.getSessionFactory();
+		 * Session session = sf.openSession();
+		 * albuns = (List<Album>)session.createQuery("from Album").list();
+		 * session.close();
+		 * 
+		 */
+		
+		/* UTILIZANDO JPA */
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("TreinaWebTesteJPA");
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		albuns = entityManager.createQuery("from Album").getResultList();
+		entityManager.close();
+		entityManagerFactory.close();
 		return albuns;
 	}
 
 	@Override
 	public Album porId(Integer id) {
 		Album album = null;
-		SessionFactory sf = HibernateSessionFactoryUtils.getSessionFactory();
-		Session session = sf.openSession();
-		album = session.get(Album.class, id);
-		session.close();
+		/*
+		 * UTILIZANDO HIBERNATE
+		 * 
+		 * SessionFactory sf = HibernateSessionFactoryUtils.getSessionFactory();
+		 * Session session = sf.openSession();
+		 * album = session.get(Album.class, id);
+		 * session.close();
+		 * 
+		 */
+		
+		/* UTILIZANDO JPA */
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("TreinaWebTesteJPA");
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		album = entityManager.find(Album.class, id);
+		entityManager.close();
+		entityManagerFactory.close();
 		return album;
 	}
 
 	@Override
 	public void inserir(Album modelo) {
-		SessionFactory sf = HibernateSessionFactoryUtils.getSessionFactory();
-		Session session = sf.openSession();
-		Transaction transacao = session.beginTransaction();
-		session.save(modelo);
-		transacao.commit();
-		session.close();
+		/*
+		 * UTILIZANDO HIBERNATE
+		 *
+		 *SessionFactory sf = HibernateSessionFactoryUtils.getSessionFactory();
+		 *Session session = sf.openSession();
+		 *Transaction transacao = session.beginTransaction();
+		 *session.save(modelo);
+		 *transacao.commit();
+		 *session.close();
+		 *
+		 */
+		
+		/* UTILIZANDO JPA */
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("TreinaWebTesteJPA");
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		entityManager.getTransaction().begin();
+		entityManager.persist(modelo);
+		entityManager.getTransaction().commit();
+		entityManager.close();
+		entityManagerFactory.close();
 	}
 	
 	@Override
 	public void atualizar(Album modelo) {
-		SessionFactory sf = HibernateSessionFactoryUtils.getSessionFactory();
-		Session session = sf.openSession();
-		Transaction transacao = session.beginTransaction();
-		session.update(modelo);
-		transacao.commit();
-		session.close();
+		/*
+		 * UTILIZANDO HIBERNATE
+		 *
+		 *SessionFactory sf = HibernateSessionFactoryUtils.getSessionFactory();
+		 *Session session = sf.openSession();
+		 *Transaction transacao = session.beginTransaction();
+		 *session.update(modelo);
+		 *transacao.commit();
+		 *session.close();
+		 */
+		
+		/* UTILIZANDO JPA */
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("TreinaWebTesteJPA");
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		entityManager.getTransaction().begin();
+		entityManager.merge(modelo);
+		entityManager.getTransaction().commit();
+		entityManager.close();
+		entityManagerFactory.close();
 	}
 
 	@Override
 	public void deletar(Album modelo) {
-		SessionFactory sf = HibernateSessionFactoryUtils.getSessionFactory();
-		Session session = sf.openSession();
-		Transaction transacao = session.beginTransaction();
-		session.delete(modelo);
-		transacao.commit();
-		session.close();
+		/*
+		 * UTILIZANDO HIBERNATE
+		 *
+		 *SessionFactory sf = HibernateSessionFactoryUtils.getSessionFactory();
+		 *Session session = sf.openSession();
+		 *Transaction transacao = session.beginTransaction();
+		 *session.delete(modelo);
+		 *transacao.commit();
+		 *session.close();
+		 */
+		
+		/* UTILIZANDO JPA */
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("TreinaWebTesteJPA");
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		entityManager.getTransaction().begin();
+		entityManager.remove(modelo);
+		entityManager.getTransaction().commit();
+		entityManager.close();
+		entityManagerFactory.close();
 	}
 	
 	
