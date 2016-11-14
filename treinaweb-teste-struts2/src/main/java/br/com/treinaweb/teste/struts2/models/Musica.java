a2d147e014a2ad2aa316231a65f345706364cbde
Renan Saraiva <renan.saraiva@gmail.com>
2016-11-14 18:08:49 -0200

    64% - Criando a entidade Musica

---
+++ treinaweb-teste-struts2/src/main/java/br/com/treinaweb/teste/struts2/models/Musica.java
@@ -0,0 +1,41 @@
+package br.com.treinaweb.teste.struts2.models;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "MUS_MUSICAS")
+public class Musica implements Serializable {
+
+	private static final long serialVersionUID = 8722113745678916996L;
+	
+	@Id
+	@GeneratedValue
+	@Column(name = "MUS_ID")
+	private int id;
+	
+	@Column(name = "MUS_NOME")
+	private String nome;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+}
